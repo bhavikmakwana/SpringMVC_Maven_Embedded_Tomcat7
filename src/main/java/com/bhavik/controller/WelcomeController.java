67344b321e999f41c0fba2328271737f772a6bf3
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2018-07-03 19:59:11 +0530

    Basic Project Set Ready With WebApp

---
+++ src/main/java/com/bhavik/controller/WelcomeController.java
@@ -0,0 +1,33 @@
+package com.bhavik.controller;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeController implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        PrintWriter out = servletResponse.getWriter();
+        out.print("Hello");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
